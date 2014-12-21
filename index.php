095b49ae6f38fd77554d3304bd376a702b55c379
TheTemporatlist <the.country.gamer@gmail.com>
2014-12-20 21:46:34 -0500

    Toying with arrays and buttons

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-	$prefix = "http://minecraft.curseforge.com/mc-mods/";
 	$GLOBALS["mods"] = array(
 		"Origin" => "countrygamer_core",
 		"Weeping Angels" => "weeping-angels",
@@ -8,20 +7,22 @@
 		"Morph Additions" => "morphadditions",
 		"Not Enough Keys" => "notenoughkeys"
 	);
-	echo "for loop<br />";
-	foreach ($mods as $mod) {
+	foreach ($GLOBALS["mods"] as $mod) {
 		$url = "http://widget.mcf.li/mc-mods/minecraft/" . $mod . ".json";
-		echo $url . "<br />";
-		#$modJson = json_decode(file_get_contents($url), true);
-		#$GLOBALS["download:" . $mod] = $modJson["download"]["url"];
+		$modJson = json_decode(file_get_contents($url), true);
+		$GLOBALS["download:" . $mod] = $modJson["download"]["url"];
 	}
-	#$GLOBALS["mods"] = $mods;
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<body>
-		<?php #echo $GLOBALS["origin"]; ?>
+		<?php
+			foreach ($GLOBALS["mods"] as $modName => $modsubname) {
+				echo "<button onclick=
+					\"window.open(" . $GLOBALS["download:" . $modsubname] . ", '_self')\"
+				>" . $modName . "</button><br />";
+			}
+
+		?>
 	</body>
 </html>
