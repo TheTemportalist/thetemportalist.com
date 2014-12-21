6538d59c09cfddf7398a2a598929b547010598bc
TheTemporatlist <the.country.gamer@gmail.com>
2014-12-20 21:23:08 -0500

    Fetching file from interwebs

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 	echo "for loop<br />";
 	foreach ($mods as $mod) {
 		$url = "http://widget.mcf.li/mc-mods/minecraft/" . $mod . ".json";
-		echo $url . "<br />";
+		echo "  " . $url . "<br />";
+		$json = file_get_contents($url);
+		echo "  " . $json . "<br />";
 	}
 	$GLOBALS["origin"] = "origin download url";
 
