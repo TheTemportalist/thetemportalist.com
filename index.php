95d7934858393303acf0e7cb5395f8ec97d7dcf4
TheTemporatlist <the.country.gamer@gmail.com>
2014-12-22 10:48:01 -0500

    Playing with table divs

--- index.php
+++ index.php
@@ -37,6 +37,13 @@
 			<img src="resources/TheTemportalist.png" style="width:437.5px; height:112.5px;">
 		</div>
 
+		<div style="width: 100%; display: table;">
+			<div style="display: table-row">
+				<div style="width: 25%; display: table-cell;"> Left </div>
+				<div style="display: table-cell;"> Right </div>
+			</div>
+		</div>
+
 		<div id="sidebar">
 
 		</div>
