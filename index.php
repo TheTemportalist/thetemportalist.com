c8252d830de741c25dad532c1db2f748529c09f6
TheTemporatlist <the.country.gamer@gmail.com>
2014-12-22 11:38:31 -0500

    Playing with dropdown buttons

--- index.php
+++ index.php
@@ -42,7 +42,17 @@
 
 		<!-- DROP DOWN -->
 		<div style="width: 100%;">
-			DROP DOWN
+			<button
+					data-dropdown="drop" aria-controls="drop",
+					aria-expanded="false" class="large alert round button dropdown">
+				Dropdown Button
+			</button>
+			<br>
+			<ul id="drop" data-dropdown-content class="f-dropdown" role="menu" aria-hidden="false" tabindex="-1">
+				<li><a href="#">This is a link</a></li>
+				<li><a href="#">This is another</a></li>
+				<li><a href="#">Yet another</a></li>
+			</ul>
 		</div>
 
 		<br />
