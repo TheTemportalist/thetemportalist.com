e5884ac79dab4329c947e180e13284236dfd601a
TheTemportalist <the.country.gamer@gmail.com>
2015-01-06 14:10:39 -0500

    (cont.)

--- dropdown.php
+++ dropdown.php
@@ -1,12 +1,16 @@
-
+<html>
+	<head>
+		<link href="ddstyle.css" rel="stylesheet" />
+	</head>
 	<body>
 		<header>
 			<div class="nav">
 				<ul class="list">
 					<li class="listItem">A</li>
-					<li class="listItem">C</li>
 					<li class="listItem">B</li>
+					<li class="listItem">C</li>
 				</ul>
 			</div>
 		</header>
 	</body>
+</html>
