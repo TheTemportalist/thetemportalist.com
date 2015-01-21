5e1f765981985c2dcd1c1ff372cfb3c392ff30f8
TheTemportalist <the.country.gamer@gmail.com>
2015-01-21 13:00:30 -0500

    Testing mod storage

---
+++ data.php
@@ -0,0 +1,24 @@
+<?php
+
+	$mods = array(
+		"Origin" => array(
+			"page" => "/page/origin"
+		),
+		"Weeping Angels" => array(
+			"page" => "/page/weeping-angels"
+		),
+		"Not Enough Keys" => array(
+			"page" => "/page/not-enough-keys"
+		),
+		"Tardis" => array(
+			"page" => "/page/tardis"
+		),
+		"Secret Rooms" => array(
+			"page" => "/page/secret-rooms"
+		),
+		"Owner Emitter" => array(
+			"page" => "/page/owner-emitter"
+		)
+	)
+
+?>
