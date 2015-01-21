402aeecc0b694026e0c141095f373d6fabaff774
TheTemportalist <the.country.gamer@gmail.com>
2015-01-20 21:27:42 -0500

    Testing json things

--- downloader.php
+++ downloader.php
@@ -1,15 +1,19 @@
 <?php
 
 function out($txt) {
-	echo $txt . "\n";
+	echo $txt . "<br />";
 }
 
-$mcv=$_GET["mcv"];
-$id=$_GET["id"];
-$v=$_GET["v"];
+$mcv = $_GET["mcv"];
+$id = $_GET["id"];
+$v = $_GET["v"];
 
 out($mcv);
 out($id);
 out($v);
+
+$json = json_decode(file_get_contents("http://widget.mcf.li/mc-mods/minecraft/" . $id . ".json"));
+var_dump($json);
+
 
 ?>
