185f19139a0a37885d9b9d6203cbd845befa9f63
TheTemportalist <the.country.gamer@gmail.com>
2015-01-20 22:31:13 -0500

    Ah. I cannot output

--- downloader.php
+++ downloader.php
@@ -8,9 +8,9 @@ $mcv = $_GET["mcv"];
 $id = $_GET["id"];
 $file = $_GET["file"];
 
-out($mcv);
-out($id);
-out($file);
+#out($mcv);
+#out($id);
+#out($file);
 
 $json = json_decode(file_get_contents("http://widget.mcf.li/mc-mods/minecraft/" . $id . ".json"), true);
 #echo '<pre>' . print_r($json, true) . '</pre>';
@@ -23,12 +23,12 @@ if (strcmp($file, 'latest') == 0) {
 else {
 	foreach ($versions as $modVersion) {
 		if (strcmp($file, $modVersion['name']) == 0) {
-			out("Found " . $modVersion['name']);
+#			out("Found " . $modVersion['name']);
 			$url = $modVersion['url'];
 		}
 	}
 }
-out($url);
+#out($url);
 header($url);
 exit;
 
