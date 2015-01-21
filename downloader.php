15e3b4db2e205d9df9ebc305cf3908f26ea202e0
TheTemportalist <the.country.gamer@gmail.com>
2015-01-20 22:23:53 -0500

    Parsing curseforge

--- downloader.php
+++ downloader.php
@@ -6,14 +6,28 @@ function out($txt) {
 
 $mcv = $_GET["mcv"];
 $id = $_GET["id"];
-$v = $_GET["v"];
+$file = $_GET["file"];
 
 out($mcv);
 out($id);
-out($v);
+out($file);
 
 $json = json_decode(file_get_contents("http://widget.mcf.li/mc-mods/minecraft/" . $id . ".json"), true);
 #echo '<pre>' . print_r($json, true) . '</pre>';
-echo '<pre>' . print_r($json['versions'][$mcv][0], true) . '</pre>';
+$versions = $json['versions'][$mcv];
+$url = ""
+
+if (strcmp($file, 'latest') == 0) {
+	$url = $versions[0];
+}
+else {
+	foreach ($versions as $modVersion) {
+		if (strcmp($file, $modVersion['name']) == 0) {
+			out("Found " . $modVersion['name']);
+			$url = $modVersion['url'];
+		}
+	}
+}
+out($url);
 
 ?>
