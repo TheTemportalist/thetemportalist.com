4643e6204427d99adf140d9b73d749a18c0c38c7
TheTemportalist <the.country.gamer@gmail.com>
2015-01-21 12:53:23 -0500

    Testing some data structures

---
+++ theme/temportalist/data.php
@@ -0,0 +1,21 @@
+<?php
+
+	$mods = array(
+		array(
+			"name" => "Origin"
+		),
+		array(
+			"name" => "Weeping Angels"
+		),
+		array(
+			"name" => "Tardis"
+		),
+		array(
+			"name" => "Secret Rooms"
+		),
+		array(
+			"name" => "Owner Emitter"
+		)
+	)
+
+?>
