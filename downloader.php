96d6ed6ac48e9aea87612de21cacd9eb6512714b
TheTemportalist <the.country.gamer@gmail.com>
2015-01-20 21:19:13 -0500

    Testing

--- downloader.php
+++ downloader.php
@@ -1,17 +1,9 @@
 <?php
 
-function out(txt) {
-	print txt . "\n";
-}
-
 mcv = $_GET["mcv"];
 id = $_GET["id"];
 v = $_GET["v"];
 
-out("...");
-out(mcv);
-out(id);
-out(v);
-out("...");
+echo mcv . "\n";
 
 ?>
