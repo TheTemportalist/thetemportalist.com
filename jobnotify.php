73f1e6e05e189b1bc347387ef6fdbe1aa7c7645f
TheTemportalist <the.country.gamer@gmail.com>
2015-02-15 16:04:23 -0500

    test

--- jobnotify.php
+++ jobnotify.php
@@ -4,10 +4,10 @@ echo "Notify me!<br />";
 $data = file_get_contents('php://input');
 #echo $entityBody;
 $dir = './jenkin_logs/';
-$date = date("j.n.Y");
+$date = ''.date("j.n.Y");
 $log = "Database Log:\n";
 $log = $log . 'String length: ' . (strlen($data) != 0);
-if (strlen($data) != 0){
+if (strlen($data) != 0) {
 	$json = json_decode($data);
 	$name = $json['name'];
 	$url = $json['build']['full_url'];
@@ -25,6 +25,6 @@ if (strlen($data) != 0){
 
 }
 echo "End";
-file_put_contents($dir  . 'Log_' . $date . '.txt', $date . ':\n' . $log . '\n,' FILE_APPEND);
+file_put_contents($dir . 'Log_' . $date . '.txt', $date . ':\n' . $log . '\n,' FILE_APPEND);
 
 ?>
