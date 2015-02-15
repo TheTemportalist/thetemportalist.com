5b2e2c12eea966f9336d63192c17dac0b71976d6
TheTemportalist <the.country.gamer@gmail.com>
2015-02-15 15:54:03 -0500

    Fixing syntax

--- jobnotify.php
+++ jobnotify.php
@@ -3,6 +3,8 @@
 echo "Notify me!";
 $data = file_get_contents('php://input');
 #echo $entityBody;
+$dir = './jenkin_logs/';
+$log = "Database Log:\n";
 if (strlen($data) != 0){
 	$json = json_decode($data);
 	$name = $json['name'];
@@ -10,18 +12,17 @@ if (strlen($data) != 0){
 	$number = $json['build']['number'];
 	$status = $json['build']['status'];
 
-	$dir = './jenkin_logs/';
 	$fileName = $name . '_' . $number . '_' . $status;
 	file_put_contents(($dir . $fileName . '_').date("j.n.Y").'.txt', $data, FILE_APPEND);
+	
 
 	if ($status === 'SUCCESS') {
-		$log = "Database Log:\n";
 		$db = new PDO('mysql:host=localhost;dbname=thetemportalist', "thetemportalist", "XXX");
 		$log .= $db;
-
-		file_put_contents(($dir . $fileName . '_').date("j.n.Y").'.txt', $log, FILE_APPEND)
+		
 	}
 
 }
+file_put_contents(($dir . '_').date("j.n.Y").'.txt', $log, FILE_APPEND);
 
 ?>
