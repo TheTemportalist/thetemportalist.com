bf8aa09356a27a4474e90586fc235e8d8b1cc576
TheTemportalist <the.country.gamer@gmail.com>
2015-02-15 15:52:09 -0500

    Logging and checking for codes

--- jobnotify.php
+++ jobnotify.php
@@ -1,11 +1,27 @@
 <?php
 
 echo "Notify me!";
-$entityBody = file_get_contents('php://input');
+$data = file_get_contents('php://input');
 #echo $entityBody;
-if (strlen($str) != 0){
-	file_put_contents('./jenkin_logs/build_'.date("j.n.Y").'.txt', $entityBody, FILE_APPEND);
-	
+if (strlen($data) != 0){
+	$json = json_decode($data);
+	$name = $json['name'];
+	$url = $json['build']['full_url'];
+	$number = $json['build']['number'];
+	$status = $json['build']['status'];
+
+	$dir = './jenkin_logs/';
+	$fileName = $name . '_' . $number . '_' . $status;
+	file_put_contents(($dir . $fileName . '_').date("j.n.Y").'.txt', $data, FILE_APPEND);
+
+	if ($status === 'SUCCESS') {
+		$log = "Database Log:\n";
+		$db = new PDO('mysql:host=localhost;dbname=thetemportalist', "thetemportalist", "XXX");
+		$log .= $db;
+
+		file_put_contents(($dir . $fileName . '_').date("j.n.Y").'.txt', $log, FILE_APPEND)
+	}
+
 }
 
 ?>
