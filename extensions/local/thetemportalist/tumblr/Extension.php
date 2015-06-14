6777c1dae62f7909da25675c124d4f5c28bbc6a0
TheTemportalist <the.country.gamer@gmail.com>
2015-06-14 15:54:57 -0400

    testing extension things

---
+++ extensions/local/thetemportalist/tumblr/Extension.php
@@ -0,0 +1,28 @@
+<?php
+	namespace Bolt\Extension\TheTemportalist\Tumblr;
+	use Symfony\Component\HttpFoundation\Request;
+	use Bolt;
+	use Silex\Application;
+
+	class Extension extends \Bolt\BaseExtension {
+
+		public function getName() {
+			return "tumblr";
+		}
+
+		public function initialize() {
+
+			// todo configged 'Notifications'
+			// get() post() or match() (for both)
+			// $this->config['path']
+
+			
+		}
+
+		public function getBlogInfo($hostname, $oauth, $secret) {
+			echo $this->app->match("api.tumblr.com/v2/blog/".$hostname."/info?api_key=".$oauth);
+		}
+
+	}
+
+?>
---
+++ extensions/local/thetemportalist/tumblr/init.php
@@ -0,0 +1,7 @@
+<?php
+	
+	use Bolt\Extension\TheTemportalist\Tumblr\Extension;
+
+	$app['extensions']->register(new Extension($app));
+
+?>
