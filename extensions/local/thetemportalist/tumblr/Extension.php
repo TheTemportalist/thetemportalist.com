27547803d1661242c0eb51be19932fb581790d4d
TheTemportalist <the.country.gamer@gmail.com>
2015-06-15 13:18:44 -0400

    catching exeptions

--- extensions/local/thetemportalist/tumblr/Extension.php
+++ extensions/local/thetemportalist/tumblr/Extension.php
@@ -74,7 +74,7 @@
 			try {
 				$result = $this->client->getBlogPosts($hostname);
 			}
-			catch (Exception $e) {
+			catch (RequestException $e) {
 				return "Invalid input";
 			}
 			return $result;
